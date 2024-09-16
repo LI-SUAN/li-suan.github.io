e627183976cbfcea96ec4c8b91a348bc80fd07e1
LI-SUAN <suan.personale@gmail.com>
2024-09-16 20:41:34 +0200

    尝试添加图标

--- assets/js/theme-switch.js
+++ assets/js/theme-switch.js
@@ -1,3 +1,9 @@
 document.getElementById('theme-toggle').addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
+    localStorage.setItem('theme', document.body.classList.contains('light-theme') ? 'light' : 'dark');
 });
+
+// 检查本地存储中的主题设置
+if (localStorage.getItem('theme') === 'light') {
+    document.body.classList.add('light-theme');
+}
